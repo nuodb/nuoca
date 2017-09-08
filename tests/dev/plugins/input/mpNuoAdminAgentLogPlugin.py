4fa5a7ba24412ad0913630f3a60d57835fe5f87b
Tom Gates <tgates@nuodb.com>
2017-09-08 16:39:18 -0400

    [OA-2046] Initial support for NuoAdminAgentLogPlugin

---
+++ tests/dev/plugins/input/mpNuoAdminAgentLogPlugin.py
@@ -0,0 +1,357 @@
+import logging
+import os
+import re
+import socket
+import subprocess
+import threading
+
+from Queue import Queue, Empty
+
+from nuoca_plugin import NuocaMPInputPlugin
+from nuoca_util import nuoca_log
+from pygrok import Grok
+
+# NuoAdminAgentLog plugin
+#
+# Example NuoAdminAgentLog plugin configuration:
+#
+# - NuoAdminAgentLog:
+#    description : Collection from NuoDB Admin logfile.
+#    adminLogfile: full path to NuoDB Admin Logfile.
+
+
+message_patterns = {
+  # NuoAgent version: 3.0.master-3780
+  "Environment.logEnv": [
+    "NuoDB %{WORD:directory} directory: %{GREEDYDATA:value}",
+    "NuoAgent version: %{GREEDYDATA:version}"],
+  "LocalServer.logNewRole": "\[.*\] Converting to (?<action>LEADER) \(term=[0-9]+, index=[0-9]+\)( %{GREEDYDATA:comment})?",
+  "PropertiesContainerImpl.logProps": "Property %{NOTSPACE:property}=%{GREEDYDATA:value}",
+  "URLPropertiesProvider.<init>": ".*properties file: %{GREEDYDATA:propertyfile}",
+  "PeerContainerImpl$LocalPeer.initStableId": "Initializing local peer.* uuid:%{UUID:stableid}",
+  "PeerContainerImpl$LocalPeer.<init>": "local bind address=%{IPORHOST:bindhost}?/%{IPORHOST:bindaddr}?:%{POSINT:bindport}, hostname=%{IPORHOST:bhostname}",
+  "PeerService$EntryListener.handleMessage": "Peering reply from \[%{IPORHOST:peerhost}?/%{IPORHOST:peeraddr}?:%{POSINT:peerport}, uuid:%{UUID:stableid}\] \(%{NUODB_AGENTTYPE:peertype}\)",
+  "TagServer$DomainJoinedRunnable.run": "Region is: %{GREEDYDATA:region}",
+  # INFO EventManager.notifyPeerEvent (serv-socket7-thread-62) Peer left: [Peer 10.3.91.5:48004 (broker)]
+  "EventManager.notifyPeerEvent": "%{NUODB_ENTITY:entity} %{NUODB_ACTIONS:action}: \[%{NUODB_PEER:_peer}\]",
+  "EventManager.nofifyDomainEvent": "%{NUODB_ACTIONS:action} %{NUODB_ENTITY:entity}",
+
+  # INFO    EventManager.notifyNodeEvent (serv-socket6-thread-163) Node joined: [Node SM db=[realtime] pid=17719 id=2 req=null (Peer 10.3.90.4:48004 (broker))]
+  # WARNING EventManager.notifyNodeEvent (serv-socket7-thread-3222) Node joined with null Peer ignored: [Node SM db=[JPMC] pid=16015 id=11 req=SMs (10.3.91.4:48005)]
+  "EventManager.notifyNodeEvent": "%{NUODB_ENTITY:entity} %{NUODB_ACTIONS:action}%{GREEDYDATA:comment}?: %{NUODB_NODE:_node}",
+  "EventManager.notifyNodeIdSet": "%{NUODB_ENTITY:entity} %{NUODB_ACTIONS:action}=%{POSINT:newnodeid} %{NUODB_NODE:_node}",
+  "EventManager.notifyNodeStateChange": "%{NUODB_ENTITY:entity} %{NUODB_ACTIONS:action} changed to %{WORD:node_state} %{NUODB_NODE:_node}",
+  "EventManager.notifyNodeFailure": [
+    "%{NUODB_ENTITY:entity} %{NUODB_ACTIONS:action} peer=\[%{NUODB_PEER}\] startId=\[%{NUMBER:startId}\]:%{NUODB_GREEDYDATA:comment}",
+    "%{NUODB_ENTITY:entity} %{NUODB_ACTIONS:action} %{NUODB_NODE:_node}: %{NUODB_GREEDYDATA:comment}"],
+  "ProcessService$ProcessReaper.reapConnected": "%{WORD:action} process with pid %{NUODB_PID:pid} and exit code %{NUMBER:exitcode}",
+  "ProcessService.nodeLeft": [
+    "%{WORD:action} up pid.%{NUMBER:pid} with exit code.%{NUMBER:exitcode}",
+    "Marking process with %{NUODB_ENTITY:entity} object %{NUODB_NODE:_node} for %{NUODB_ACTIONS:action}"],
+  "NuoAgent.logReady": "%{GREEDYDATA:comment}; agent is ready",
+  "NuoAgent.shutdown": "%{GREEDYDATA:comment}"
+
+}
+
+nuodb_patterns = {
+  "NUODB_NODE": "\[Node %{WORD:node_type} db=\[%{WORD:dbname}\] pid=%{POSINT:pid} id=%{INT:nodeid} req=%{WORD:nodegroup} \((%{IPORHOST:peeraddr}:%{NUMBER:nodeport}|%{NUODB_PEER:_peer})\)\]",
+  "NUODB_ENTITY": "([nN]ode|[pP]eer|[dD]omain)",
+  "NUODB_ACTIONS": "([Jj]oined|left|state|setId|failed|reaping)",
+  "NUODB_AGENTTYPE": "(broker|agent)",
+  "NUODB_PEER": "((Local)?Peer %{IPORHOST:peeraddr}:%{POSINT:peerport} \(%{NUODB_AGENTTYPE:peertype}\)|local)",
+  "NUODB_PID": "[0-9]+(,[0-9]{3})?",
+  # multi-line greedy
+  "NUODB_GREEDYDATA": "(.*\n?)+"
+}
+
+# replace patterns with Grok
+grok_patterns = {}
+for logger, patterns in message_patterns.iteritems():
+  if type(patterns) is not str:
+    grok_patterns[logger] = [
+      Grok("^%s" % (pattern), custom_patterns=nuodb_patterns) for pattern in
+      patterns]
+  else:
+    grok_patterns[logger] = [
+      Grok("^%s" % (patterns), custom_patterns=nuodb_patterns)]
+
+
+def setregion(event):
+  # do nothing for regions
+  pass
+
+def updatepeer(event):
+  # do nothing for peers
+  pass
+
+def logevent(event):
+  if 'logger' not in event:
+    event['logger'] = None
+
+def lifecycle(event):
+  if event['action'] is not None:
+    if event['entity'] == 'Node':
+      if event['action'] == 'setId':
+        event['node_state'] = "STARTED"
+      elif event['action'] == 'joined':
+        event['node_state'] = "STARTING"
+      elif event['action'] == 'left':
+        event['node_state'] = "EXITTED"
+      elif event['action'] == 'failed':
+        event['node_state'] = "FAILED"
+      if 'observed' in event and event['observed'] is not None:
+        event['action'] += "/" + event['observed']
+  else:
+    nuoca_log(logging.WARNING,
+              "NuoAdminAgentLog plugin event without action: %8s %20s %s" % (
+              event['loglevel'], event['logger'], event['message']))
+
+def exitcycle(event):
+  """
+  %{WORD:op} up pid %{POSINT:pid} with exit code %{POSINT:exitcode}
+  %{WORD:op} process with pid %{NUODB_PID:pid} and exit code %{POSINT:exitcode}
+  """
+  if 'action' in event:
+    if 'exitcode' in event:
+      event['comment'] = "exit code " + event['exitcode']
+    event['entity'] = "Node"
+    event['pid'] = event['pid'].replace(',', '')
+    event['node_state'] = "FINISHED"
+    lifecycle(event)
+
+
+def checkproperty(event):
+  # Do nothing for checkproperty
+  pass
+
+def mapstableid(event):
+  # Do nothing for mapstableid
+  pass
+
+def peerhandle(event):
+  if 'stableid' in event:
+    stableid = event['stableid']
+    event['iporaddr'] = event['peerhost']
+    event['port'] = event['peerport']
+    event['iporaddr'] = event['peeraddr']
+    mapstableid(event)
+
+def agentevent(event):
+  if 'logger' in event:
+    logger = event['logger']
+    event['entity'] = "Peer"
+    if logger.endswith('logReady'):
+      event['node_state'] = "STARTED"
+      event['action'] = 'State'
+    elif logger.endswith('shutdown'):
+      event['node_state'] = "EXITTED"
+      event['action'] = 'Left'
+
+def updateenv(event):
+  if 'version' in event:
+    event['entity'] = "Peer"
+    event['action'] = 'Joined'
+    event['node_state'] = "STARTING"
+    event['comment'] = event['version']
+
+def raftrole(event):
+  event['entity'] = 'Peer'
+
+
+event_handlers = {
+  "PeerContainerImpl$LocalPeer": updatepeer,
+  "Environment.logEnv": updateenv,
+  "PropertiesContainerImpl.logProps": checkproperty,
+  "TagServer$DomainJoinedRunnable.run": setregion,
+  "ProcessService": exitcycle,
+  "ProcessService$ProcessReaper": exitcycle,
+  "EventManager": lifecycle,
+  "PeerService$EntryListener.handleMessage": peerhandle,
+  "NuoAgent": agentevent,
+  # "LocalServer.logNewRole"                  : raftrole
+}
+
+def process(event):
+  logevent(event)
+  if 'logger' in event and event['logger'] is not None:
+    elogger = event['logger']
+    found = False
+    if elogger in grok_patterns:
+      patterns = grok_patterns[elogger]
+      emsg = event['message']
+      for grok in patterns:
+        nevent = grok.match(emsg)
+        if nevent:
+          found = True
+          event.update(nevent)
+          break
+      if not found:
+        logging.debug("%s %s" % (elogger, emsg))
+    if elogger in event_handlers:
+      event_handlers[elogger](event)
+    elif found:
+      logger = elogger[:elogger.rfind('.')]
+      if logger in event_handlers:
+        event_handlers[logger](event)
+    print event
+
+
+class Process:
+  def __init__(self):
+    grok_patterns = [
+      "%{TIMESTAMP_ISO8601:timestamp} %{LOGLEVEL:loglevel} %{NOTSPACE:logger} %{NOTSPACE:thread} +%{GREEDYDATA:message}",
+      "%{TIMESTAMP_ISO8601:timestamp} \[%{POSINT:processid}\] +%{GREEDYDATA:message}",
+      "%{TIMESTAMP_ISO8601:timestamp} %{LOGLEVEL:loglevel} +%{GREEDYDATA:message}"
+    ]
+    self.groks = [Grok(pattern) for pattern in grok_patterns]
+    self.event = None
+
+  def next(self, line):
+    nevent = None
+    for pattern in self.groks:
+      nevent = pattern.match(line[:-1])
+      if nevent:
+        break
+    if nevent is None and self.event is not None:
+      self.event['message'] += '\n' + line[:-1]
+    else:
+      if self.event is not None:
+        process(self.event)
+        self.event = None
+      if nevent is not None:
+        self.event = nevent
+    if not self.event:
+      nuoca_log(logging.WARNING,
+                "NuoAdminAgentLog plugin skipping line: %s" % line)
+    pass
+
+  def complete(self):
+    if self.event is not None:
+      process(self.event)
+
+
+class MPNuoAdminAgentLog(NuocaMPInputPlugin):
+  def __init__(self, parent_pipe):
+    super(MPNuoAdminAgentLog, self).__init__(parent_pipe, 'NuoAdminAgentLog')
+    self._config = None
+    self._enabled = False
+    self._agentLogfile = None
+    self._tail_thread = None
+    self._process_thread = None
+    self._agentLogQueue = Queue()
+    self._tail_subprocess = None
+    self._line_counter = 0
+    self._lines_processed = 0
+    self._local_hostname = socket.gethostname()
+    self._host_uuid_shortname = False
+    self._host_shortid = None
+    self._nuoAdminAgentLog_collect_queue = []
+
+  @property
+  def nuoAdminAgentLog_collect_queue(self):
+    return self._nuoAdminAgentLog_collect_queue
+
+  def _tail_forever_thread(self, filename):
+    self._tail_subprocess = \
+      subprocess.Popen(["tail", "-c", "+0", "-f", filename],
+                        stdout=subprocess.PIPE)
+    while self._enabled:
+      line = self._tail_subprocess.stdout.readline()
+      if line:
+        self._line_counter += 1
+        self._agentLogQueue.put(line)
+    nuoca_log(logging.INFO,
+              "NuoAdminAgentLog plugin tail_forever_thread "
+              "completed %s lines" % str(self._line_counter))
+
+  def _process_agent_log_thread(self):
+    process = Process()
+    line = None
+    while self._enabled:
+      try:
+        line = self._agentLogQueue.get(block=True, timeout=10)
+      except Empty:
+        line = None
+      if line:
+        process.next(line)
+        self._lines_processed += 1
+      if process.event:
+        self._nuoAdminAgentLog_collect_queue.append(process.event)
+      if not line:
+        process.complete()
+        if process.event:
+          self._nuoAdminAgentLog_collect_queue.append(process.event)
+          process.event = None
+    nuoca_log(logging.INFO,
+              "NuoAdminAgentLog plugin process_agent_log_thread "
+              "completed %s lines" % str(self._lines_processed))
+
+
+  def startup(self, config=None):
+    uuid_hostname_regex = \
+      '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}-'
+    try:
+      self._config = config
+
+      # Validate the configuration.
+      required_config_items = ['agentLogfile']
+      if not self.has_required_config_items(config, required_config_items):
+        return False
+      nuoca_log(logging.INFO, "NuoAdminAgentLog plugin config: %s" %
+                str(self._config))
+
+      # For Coach hostnames in the format: uuid-shortId
+      if 'host_uuid_shortname' in config:
+        self._host_uuid_shortname = config['host_uuid_shortname']
+
+      if self._host_uuid_shortname:
+        m2 = re.search(uuid_hostname_regex, self._local_hostname)
+        if m2:
+          self._host_shortid = self._local_hostname[37:]
+
+      self._agentLogfile = os.path.expandvars(config['agentLogfile'])
+      self._enabled = True
+      self._tail_thread = threading.Thread(target=self._tail_forever_thread,
+                                      args=(self._agentLogfile,))
+      self._tail_thread.daemon = True
+      self._tail_thread.start()
+
+      self._process_thread = \
+        threading.Thread(target=self._process_agent_log_thread)
+      self._process_thread.daemon = False
+      self._process_thread.start()
+
+      return True
+
+    except Exception as e:
+      nuoca_log(logging.ERROR, "NuoAdminAgentLog Plugin: %s" % str(e))
+      return False
+
+  def shutdown(self):
+    self.enabled = False
+    if self._tail_subprocess:
+      self._tail_subprocess.terminate()
+      self._tail_subprocess = None
+    if self._process_thread:
+      self._process_thread.join()
+
+  def collect(self, collection_interval):
+    rval = None
+    try:
+      nuoca_log(logging.DEBUG, "Called collect() in NuoAdminAgentLog Plugin process")
+      base_values = super(MPNuoAdminAgentLog, self).collect(collection_interval)
+      rval = []
+      rval.append(base_values)
+      rval['Hostname'] = self._local_hostname
+      if self._host_shortid:
+        rval['HostShortID'] = self._host_shortid
+      collection_count = len(self._nuoAdminAgentLog_collect_queue)
+      if not collection_count:
+        return rval
+
+      for i in range(collection_count):
+        collected_dict = self._nuoAdminAgentLog_collect_queue.pop(0)
+        rval.append(collected_dict)
+    except Exception as e:
+      nuoca_log(logging.ERROR, str(e))
+    return rval
---
+++ tests/dev/unit/test_nuoAdminAgentLogPlugin.py
@@ -0,0 +1,93 @@
+from __future__ import print_function
+
+import os
+import unittest
+import nuoca_util
+import time
+
+from nuoca import NuoCA
+from yapsy.MultiprocessPluginManager import MultiprocessPluginManager
+from nuoca_plugin import NuocaMPInputPlugin, NuocaMPOutputPlugin, \
+    NuocaMPTransformPlugin
+
+from tests.dev.plugins.input.mpNuoAdminAgentLogPlugin import MPNuoAdminAgentLog
+
+
+class TestInputPlugins(unittest.TestCase):
+  def _MPNuoAdminAgentLogPluginTest(self):
+    nuoca_util.initialize_logger("/tmp/nuoca.test.log")
+    nuoAdminAgentLog_plugin = MPNuoAdminAgentLog(None)
+    self.assertIsNotNone(nuoAdminAgentLog_plugin)
+    nuoAdminAgentLog_plugin.shutdown()
+
+  def _MultiprocessPluginManagerTest(self):
+    child_pipe_timeout = 600
+    self.manager.setCategoriesFilter({
+        "Input": NuocaMPInputPlugin,
+        "Ouput": NuocaMPOutputPlugin,
+        "Transform": NuocaMPTransformPlugin
+    })
+
+    self.manager.collectPlugins()
+    all_plugins = self.manager.getAllPlugins()
+    self.assertTrue(all_plugins)
+    self.assertTrue(len(all_plugins) > 0)
+    nuoAdminAgentLog_plugin = None
+    for a_plugin in all_plugins:
+      self.manager.activatePluginByName(a_plugin.name, 'Input')
+      self.assertTrue(a_plugin.is_activated)
+      if a_plugin.name == 'NuoAdminAgentLog':
+        nuoAdminAgentLog_plugin = a_plugin
+    self.assertIsNotNone(nuoAdminAgentLog_plugin)
+
+    config = {'agentLogfile': '/home/tgates/coach_results.save/00f4e05b-403c-4f63-887e-c8331ef4087a/hosts/r0db0/agent.log'}
+    #config = {'agentLogfile': '/home/tgates/coach_results.save/aa3657a3-d697-4075-8470-6015d906dd47/hosts/r0db6/agent.log'}
+    plugin_msg = {'action': 'startup', 'config': config}
+    plugin_resp_msg = None
+    nuoAdminAgentLog_plugin.plugin_object.child_pipe.send(plugin_msg)
+    if nuoAdminAgentLog_plugin.plugin_object.child_pipe.poll(child_pipe_timeout):
+      plugin_resp_msg = nuoAdminAgentLog_plugin.plugin_object.child_pipe.recv()
+    self.assertIsNotNone(plugin_resp_msg)
+    self.assertEqual(0, plugin_resp_msg['status_code'])
+
+    time.sleep(30)
+
+    plugin_msg = {'action': 'collect', 'collection_interval': 3}
+    plugin_resp_msg = None
+    nuoAdminAgentLog_plugin.plugin_object.child_pipe.send(plugin_msg)
+    if nuoAdminAgentLog_plugin.plugin_object.child_pipe.poll(child_pipe_timeout):
+      plugin_resp_msg = nuoAdminAgentLog_plugin.plugin_object.child_pipe.recv()
+    self.assertIsNotNone(plugin_resp_msg)
+    resp_values = plugin_resp_msg['resp_values']
+    self.assertIsNotNone(resp_values)
+    self.assertEqual(0, plugin_resp_msg['status_code'])
+    self.assertIsNotNone(resp_values['collected_values'])
+    self.assertTrue(type(resp_values['collected_values']) is list)
+    self.assertIsNotNone(resp_values['collected_values'][0]['nuoca_plugin'])
+    self.assertIsNotNone(resp_values['collected_values'][0]['collect_timestamp'])
+
+    plugin_msg = {'action': 'shutdown'}
+    nuoAdminAgentLog_plugin.plugin_object.child_pipe.send(plugin_msg)
+
+    plugin_msg = {'action': 'exit'}
+    plugin_resp_msg = None
+    nuoAdminAgentLog_plugin.plugin_object.child_pipe.send(plugin_msg)
+
+    for a_plugin in all_plugins:
+      self.manager.deactivatePluginByName(a_plugin.name, 'Input')
+      self.assertFalse(a_plugin.is_activated)
+
+  def runTest(self):
+    topdir = nuoca_util.get_nuoca_topdir()
+    input_plugin_dir = os.path.join(topdir, "tests/dev/plugins/input")
+    dir_list = [input_plugin_dir]
+    self._MPNuoAdminAgentLogPluginTest()
+    self.manager = MultiprocessPluginManager(
+        directories_list=dir_list,
+        plugin_info_ext="multiprocess-plugin")
+    self._MultiprocessPluginManagerTest()
+
+  def tearDown(self):
+    NuoCA.kill_all_plugin_processes(self.manager, timeout=1)
+
+
