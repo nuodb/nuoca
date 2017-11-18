b999368232be79e7673de1958c5d7a5d02d42f8d
tgates <9892-Htig>
2017-11-18 15:30:03 -0500

    [ OA-2744 ] Add pre-built zabbix3 to NuoCA

--- tests/etc/elastic_search/es_test_create_index.sh
+++ tests/etc/elastic_search/es_test_create_index.sh
@@ -4,5 +4,5 @@ set -e
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-curl -XPUT http://localhost:9200/es_test?pretty -d @${THIS_DIR}/../../../etc/elastic_earch/es_mappings
+curl -XPUT http://localhost:9200/es_test?pretty -d @${THIS_DIR}/../../../etc/elastic_search/es_mappings
 
