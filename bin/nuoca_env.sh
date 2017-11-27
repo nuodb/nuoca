ff9ab3975c9f96b5c258f1e8b635ea2a2ce5fe92
Tom Gates <tgates@nuodb.com>
2017-11-27 16:07:44 -0500

    [OA-2766] Changes for package installs and GitHub devo

--- bin/nuoca_env.sh
+++ bin/nuoca_env.sh
@@ -8,5 +8,6 @@ if [ -d ${NUOCA_HOME}/python ]; then
   export PATH=${NUOCA_HOME}/python/bin:${PATH}
   export PYTHONHOME=${NUOCA_HOME}/python:${NUOCA_HOME}/python/x86_64-linux
 fi
+export PATH=${PATH}:${NUOCA_HOME}/zabbix/bin
 export PYTHONPATH=${NUOCA_HOME}/src:${NUOCA_HOME}:${NUOCA_HOME}/lib
 export NUOADMINAGENTLOGCONFIG=${NUOCA_HOME}/etc/logstash/nuoadminagentlog.conf
