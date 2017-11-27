07dd6704c72689eea6e8b92e626176e15ac9d01d
Tom Gates <tgates@nuodb.com>
2017-11-27 07:06:22 -0500

    [OA-2766] Changes for package installs and GitHub devo

--- bin/nuoca_env.sh
+++ bin/nuoca_env.sh
@@ -4,7 +4,9 @@ export NUOCA_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 export LOGSTASH_HOME=${NUOCA_ROOT}/logstash
 export NUODB_AGENTPORT=48004
 export NUODB_DOMAIN_PASSWORD=bird
-export PATH=${NUOCA_ROOT}/python/bin:${PATH}
-export PYTHONHOME=${NUOCA_ROOT}/python:${NUOCA_ROOT}/python/x86_64-linux
+if [ -d ${NUOCA_ROOT}/python ]; then
+  export PATH=${NUOCA_ROOT}/python/bin:${PATH}
+  export PYTHONHOME=${NUOCA_ROOT}/python:${NUOCA_ROOT}/python/x86_64-linux
+fi
 export PYTHONPATH=${NUOCA_ROOT}/src:${NUOCA_ROOT}:${NUOCA_ROOT}/lib 
 export NUOADMINAGENTLOGCONFIG=${NUOCA_ROOT}/etc/logstash/nuoadminagentlog.conf
