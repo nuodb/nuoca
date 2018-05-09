1ac5fc838442c289d90b5a705a3cae603c8e4267
Paul Smith <psmith@nuodb.com>
2018-05-09 14:38:55 -0400

    [DB-22276] bin/setup_logstash.sh: Provide better error handling.

--- bin/setup_logstash.sh
+++ bin/setup_logstash.sh
@@ -5,31 +5,64 @@
 # This source code is licensed under the MIT license found in the LICENSE
 # file in the root directory of this source tree.
 
+LOGSTASH_VERSION=5.6.4    # Same as logstash version in NuoDB bld extern.
+
 # Find the NuoCA home directory.
 CMD=${0##*/}
 DIR=`cd "${0%$CMD}." && pwd`
 NUOCA_HOME=${DIR%/*}
 
-cd "${NUOCA_HOME}"
+die() { echo "$*" 1>&2; exit 1; }
+
+cd "$NUOCA_HOME" || die "Can't access $NUOCA_HOME"
 
 # Setup logstash.
 
-LOGSTASH_VERSION=5.6.4    # Same as Logstach version in NuoDB bld pkg.
-LOGSTASH_HOME=${NUOCA_HOME}/extern/logstash
-LOGSTASH_TARBALL_NAME=logstash-${LOGSTASH_VERSION}.tar.gz
-LOGSTASH_TARBALL_PATH=${NUOCA_HOME}/extern/${LOGSTASH_TARBALL_NAME}
-LOGSTASH_URL=https://artifacts.elastic.co/downloads/logstash/${LOGSTASH_TARBALL_NAME}
+LOGSTASH_ROOT="$NUOCA_HOME/extern"
+mkdir -p "$LOGSTASH_ROOT"
+
+LOGSTASH_HOME="$LOGSTASH_ROOT/logstash"
+
+LOGSTASH_NAME="logstash-$LOGSTASH_VERSION"
+LOGSTASH_PATH="$LOGSTASH_ROOT/$LOGSTASH_NAME"
+LOGSTASH_TARBALL_NAME="$LOGSTASH_NAME.tar.gz"
+LOGSTASH_TARBALL_PATH="$LOGSTASH_PATH.tar.gz"
+LOGSTASH_URL="https://artifacts.elastic.co/downloads/logstash/$LOGSTASH_TARBALL_NAME"
 
 
 # Download, if needed
-if [ ! -f "${LOGSTASH_TARBALL_PATH}" ]; then
-  echo "Logstash tarball not found: ${LOGSTASH_TARBALL_PATH}"
-  echo "Downloading logstash: ${LOGSTASH_VERSION}"
-  curl -s -L -o "${LOGSTASH_TARBALL_PATH}" "${LOGSTASH_URL}"
+if [ ! -f "$LOGSTASH_TARBALL_PATH" ]; then
+  echo "Logstash tarball not found: $LOGSTASH_TARBALL_PATH"
+  echo "Downloading $LOGSTASH_URL"
+  if ! curl -s -L -o "$LOGSTASH_TARBALL_PATH" "$LOGSTASH_URL"; then
+      rm -f "$LOGSTASH_TARBALL_PATH"
+      die "Download failed.  Logstash not installed."
+  fi
 fi
 
-echo "Untaring logstash tarball"
-(cd extern; tar -xzf "${LOGSTASH_TARBALL_PATH}")
-rm -fr "${NUOCA_HOME}/extern/${LOGSTASH_HOME}"
-mv "${NUOCA_HOME}/extern/logstash-${LOGSTASH_VERSION}" "${NUOCA_HOME}/extern/logstash"
-chown -R --reference="${NUOCA_HOME}" "${NUOCA_HOME}/extern/logstash"
+
+# Extract the tarball
+echo "Extracting $LOGSTASH_TARBALL_PATH"
+rm -rf "$LOGSTASH_PATH"
+if ! (cd "$LOGSTASH_ROOT" && tar -xzf "$LOGSTASH_TARBALL_PATH"); then
+    rm -f "$LOGSTASH_TARBALL_PATH"
+    die "Extract failed.  Logstash not installed."
+fi
+
+# Remove any previous install in a safe way
+if [ -e "$LOGSTASH_HOME" ]; then
+    rm -rf "$LOGSTASH_HOME".old
+    mv "$LOGSTASH_HOME" "$LOGSTASH_HOME".old \
+       || die "Failed to rename $LOGSTASH_HOME"
+    rm -rf "$LOGSTASH_HOME".old || echo "Failed to remove $LOGSTASH_HOME".old
+fi
+
+mv "$LOGSTASH_PATH" "$LOGSTASH_HOME" \
+   || die "Failed to rename $LOGSTASH_PATH to $LOGSTASH_HOME"
+
+# This shouldn't be needed: this script is generally invoked by the NuoDB
+# agent which runs as the NuoDB user account, not as root.
+chown -R --reference="$NUOCA_HOME" "$LOGSTASH_HOME"
+
+echo "Logstash available at $LOGSTASH_HOME"
+exit 0
