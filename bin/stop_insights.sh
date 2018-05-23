5d45be252354b5eca712569e660c75835c4d6196
Tom Gates <tgates@nuodb.com>
2018-05-23 11:25:40 -0400

    [OA-2952] Revert previous commit for OA-2952
    This reverts commit 479a32e888669f7cf9c51b42f846a9d26f12ca44.

--- bin/stop_insights.sh
+++ bin/stop_insights.sh
@@ -16,14 +16,8 @@ NUOCA_HOME=${DIR%/*}
 if [ -f "${NUODB_RUNDIR}/nuoca.pid" ]; then
   NUOCA_PID=`cat "${NUODB_RUNDIR}/nuoca.pid"`
   kill $NUOCA_PID
-  count=0
-  while [ $count -lt 15 ]; do
-    [ "$(ps -ef | grep nuoca | awk '{print $2}')" ] || break
-    (( count++ ))
-    sleep 1
-  done
-  for x in $(ps -ef | grep nuoca | awk '{print $2}'); { kill -9 $x &> /dev/null; }
-  echo "Shutdown Insights PID: $NUOCA_PID"
+  echo "Killed Insights PID: $NUOCA_PID"
   rm -f "${NUODB_RUNDIR}/nuoca.pid"
   "${NUOCA_HOME}/bin/stop_zabbix_agentd.sh"
 fi
+
