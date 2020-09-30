76e673980cc2efc18c5b86e789fc7759a5c3118f
Paul Smith <paul@mad-scientist.net>
2020-09-30 13:40:10 -0400

    Update nuodb_setup for delivery in the nuodb-client package (#16)
    Enhance the setup script to allow it to work in all the cases:
      * Run from within a Git workspace
      * Run from the PyPI distributed package
      * Run from the NuoDB Client package
      * Run from the NuoDB Database package (this is obsolete)

--- etc/nuoca_setup.sh
+++ etc/nuoca_setup.sh
@@ -7,50 +7,55 @@
 
 : "${NUOCA_HOME:?ERROR: NUOCA_HOME is not set!}"
 
-# See if we're running nuoca from within a NuoDB package
-if [ -z "$NUODB_HOME" ]; then
-    case $NUOCA_HOME in
-        (*/etc/nuoca)
-            if [ -f "$NUOCA_HOME/../../etc/nuodb_setup.sh" ]; then
-                NUODB_HOME=${NUOCA_HOME%/etc/nuoca}
-            fi ;;
-        (*) : not NuoDB ;;
-    esac
-    : ${NUODB_HOME:=/opt/nuodb}
+if [ -f "$NUOCA_HOME"/etc/nuodb_setup.sh ]; then
+    _home="$NUOCA_HOME"
+elif [ -n "$NUOCLIENT_HOME" ] && [ -f "$NUOCLIENT_HOME"/etc/nuodb_setup.sh ]; then
+    _home="$NUOCLIENT_HOME"
+elif [ -n "$NUODB_HOME" ] && [ -f "$NUODB_HOME"/etc/nuodb_setup.sh ]; then
+    _home="NUODB_HOME"
+elif [ -f /opt/nuodb/etc/nuodb_setup.sh ]; then
+    _home=/opt/nuodb
+else
+    echo "Cannot locate NuoDB installation"
+    exit 1
 fi
 
-[ -f "$NUODB_HOME/etc/nuodb_setup.sh" ] || { echo "Invalid NUODB_HOME: $NUODB_HOME"; exit 1; }
-. "${NUODB_HOME}/etc/nuodb_setup.sh"
+. "$_home"/etc/nuodb_setup.sh
 
 : ${NUODB_PORT:=48004}
 NUODB_DOMAIN_PASSWORD=${DOMAIN_PASSWORD:-bird}
 
-: ${LOGSTASH_HOME:="${NUOCA_HOME}/extern/logstash"}
+: ${LOGSTASH_HOME:="$NUOCA_HOME/extern/logstash"}
 : ${NUOADMIN_API_SERVER_NONSSL:="http://localhost:8888"}
 : ${NUOADMIN_API_SERVER_SSL:="https://localhost:8888"}
 
-
-if [ -z "${NUODB_NUOCA_CLIENT_KEY}" ]; then
-  if [ -f "${NUODB_CFGDIR}/keys/nuodb_insights.pem" ]; then
-    export NUODB_NUOCA_CLIENT_KEY="${NUODB_CFGDIR}/keys/nuodb_insights.pem"
-  else
-    if [ -f  "${NUODB_CFGDIR}/keys/nuocmd.pem" ]; then
-      export NUODB_NUOCA_CLIENT_KEY="${NUODB_CFGDIR}/keys/nuocmd.pem"
+if [ -z "$NUODB_NUOCA_CLIENT_KEY" ]; then
+    if [ -f "$NUODB_CFGDIR/keys/nuodb_insights.pem" ]; then
+        export NUODB_NUOCA_CLIENT_KEY="$NUODB_CFGDIR/keys/nuodb_insights.pem"
+    elif [ -f  "$NUODB_CFGDIR/keys/nuocmd.pem" ]; then
+        export NUODB_NUOCA_CLIENT_KEY="$NUODB_CFGDIR/keys/nuocmd.pem"
     fi
-  fi
 fi
 
-PATH="${PATH}:${NUOCA_HOME}/extern/zabbix/bin"
-PYTHONPATH="${NUOCA_HOME}/src:${NUOCA_HOME}:${NUOCA_HOME}/lib:${NUODB_ROOT}/drivers/pynuoadmin"
-NUOADMINAGENTLOGCONFIG="${NUOCA_HOME}/etc/logstash/nuoadminagentlog.conf"
+NUOADMINAGENTLOGCONFIG="$NUOCA_HOME/etc/logstash/nuoadminagentlog.conf"
 
-# Are we running Python included in the NuoDB package?
-_pypath="${NUOCA_HOME%/*}/python"
-PYTHONCMD="$_pypath/nuopython"
-if [ -x "$PYTHONCMD" ]; then
-    PATH="${_pypath}/bin:${PATH}"
-    PYTHONHOME="${_pypath}:${_pypath}/x86_64-linux"
+PATH="$PATH:$NUOCA_HOME/extern/zabbix/bin"
+
+# This is if we're running from a Git repo or from PyPI
+[ -d "$NUOCA_HOME/pynuoca/lib" ] \
+    && PYTHONPATH="${PYTHONPATH:+$PYTHONPATH:}$NUOCA_HOME"
+
+# This is if we're running from an unpacked NuoDB package
+[ -d "$_home/etc/python/site-packages" ] \
+    && PYTHONPATH="${PYTHONPATH:+$PYTHONPATH:}$_home/etc/python/site-packages"
+
+# See if we can locate nuopython
+if [ -x "$_home"/etc/python/nuopython ]; then
+    PYTHONCMD="$_home"/etc/python/nuopython
+elif [ -n "$NUODB_HOME" ] && [ -x "NUODB_HOME"/etc/python/nuopython ]; then
+    PYTHONCMD="$NUODB_HOME"/etc/python/nuopython
 else
     PYTHONCMD=python
 fi
-unset _pypath
+
+unset _home
